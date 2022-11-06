3b33fd15910b91bae120d8399a291aeb15e5bfd1
DKKs <eye.q.21@gmail.com>
2022-11-07 05:08:54 +0700

    [Add] primevue

---
+++ plugins/primevue.ts
@@ -0,0 +1,9 @@
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+  nuxtApp.vueApp.component('Button', Button)
+  // other components that you need
+})
