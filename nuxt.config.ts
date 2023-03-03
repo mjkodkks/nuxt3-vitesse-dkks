a3d00a0c1856698172632a2c50cb31ce171c6d65
DKKs <eye.q.21@gmail.com>
2023-03-03 12:49:06 +0700

    [Add] more config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ],
+    },
+  },
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    prerender: {
+      crawlLinks: false,
+      routes: ['/'],
+      ignore: ['/hi'],
+    },
+  },
   modules: [
     '@vueuse/nuxt',
     '@pinia/nuxt',
