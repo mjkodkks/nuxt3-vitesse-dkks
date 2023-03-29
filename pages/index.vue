140428cd96fdf6f28b1f2da001c17d776c76c62c
DKKs <eye.q.21@gmail.com>
2023-03-29 23:23:11 +0700

    [Add] tailwindcss 3.3.0

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,14 @@
         </div>
       </template>
     </Suspense>
+    <div class="flex justify-center">
+      <ul
+        class="list-image-[url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQiIGhlaWdodD0iMTIiIHZpZXdCb3g9IjAgMCAxNCAxMiIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiBmaWxsPSIjMzhiZGY4Ij48cGF0aCBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMy42ODUuMTUzYS43NTIuNzUyIDAgMCAxIC4xNDMgMS4wNTJsLTggMTAuNWEuNzUuNzUgMCAwIDEtMS4xMjcuMDc1bC00LjUtNC41YS43NS43NSAwIDAgMSAxLjA2LTEuMDZsMy44OTQgMy44OTMgNy40OC05LjgxN2EuNzUuNzUgMCAwIDEgMS4wNS0uMTQzWiIgLz48L3N2Zz4=)]">
+        <li class="text-left">tailwindcss v3.3.0</li>
+        <li class="text-left">nuxt v3.3.1</li>
+        <!-- ... -->
+      </ul>
+    </div>
     <InputEntry />
   </div>
 </template>
