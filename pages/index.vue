9ad89ebd2cd74bd9a98c5c4160646ab50bd2cebc
DKKs <eye.q.21@gmail.com>
2024-01-31 15:43:40 +0700

    [Add] nuxt v3.10.0

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@ const whatEnv = config.public.WHAT_ENV
           tailwindcss v3.4.1
         </li>
         <li class="text-left">
-          nuxt v3.9.3
+          nuxt v3.10.0
         </li>
         <li v-if="whatEnv">
           what env : <span class="text-green-400">{{ whatEnv }}</span>
