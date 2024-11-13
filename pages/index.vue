f526e037fb53b438fad2e61865b55380ffa5576a
DKKs <eye.q.21@gmail.com>
2024-11-14 01:07:12 +0700

    [Add] nuxt v3.14.159 and update deps

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@ const whatEnv = config.public.WHAT_ENV
           tailwindcss v3.4.14
         </li>
         <li class="text-left">
-          nuxt v3.13.2
+          nuxt v3.14.159
         </li>
         <li v-if="whatEnv">
           what env : <span class="text-green-400">{{ whatEnv }}</span>
